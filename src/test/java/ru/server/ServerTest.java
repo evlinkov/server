d5d793a2945c6c89cbeccd353493efc4e9af74fc
Evgeniy Linkov <evlinkov@gmail.com>
2017-05-08 05:20:25 +0300

    testServer

--- src/test/java/ru/server/ServerTest.java
+++ src/test/java/ru/server/ServerTest.java
@@ -1,31 +1,32 @@
 package ru.server;
 
-import org.junit.Ignore;
+import java.net.URL;
 import org.junit.Test;
-
-import java.io.BufferedReader;
+import org.junit.Ignore;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.io.InputStream;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
+import static org.junit.Assert.assertEquals;
 
 public class ServerTest {
 
-    /*private static final String IP = "localhost";
+    private static final String IP = "localhost";
     private static final String PORT = "5555";
 
     @Test
     @Ignore
-    public void testServer() throws Exception {
-
-        URL url = new URL("http://" + IP + ":" + PORT + "/api/test");
+    public void testGET() throws Exception {
+        URL url = new URL("http://" + IP + ":" + PORT + "/data");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
 
         InputStream inputStream;
         int responseCode = connection.getResponseCode();
 
-        System.out.println(responseCode);
+        assertEquals(responseCode, 200);
 
         inputStream = connection.getInputStream();
 
@@ -37,8 +38,37 @@ public class ServerTest {
         while ((inputLine = input.readLine()) != null) {
             data.append(inputLine);
         }
+        assertEquals(data.toString(), "all is ok");
+    }
+
+    @Test
+    @Ignore
+    public void testPOST() throws Exception {
+        String receipt = new String(Files.readAllBytes(Paths.get("./src/main/resources/RequestExample.xml")));
+        URL url = new URL("http://" + IP + ":" + PORT + "/getCategories");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/xml");
+        connection.setDoOutput(true);
+        byte[] bytes = receipt.getBytes("UTF-8");
+        connection.getOutputStream().write(bytes);
+
+        InputStream inputStream;
+        int responseCode = connection.getResponseCode();
+        inputStream = connection.getInputStream();
+        assertEquals(responseCode, 200);
+
+        StringBuilder data = new StringBuilder();
+        String inputLine;
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(
+                inputStream));
+        while ((inputLine = input.readLine()) != null) {
+            data.append(inputLine);
+        }
         System.out.println(data);
 
-    }*/
+    }
 
 }
